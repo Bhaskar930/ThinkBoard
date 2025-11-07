761babb4405d1d3a788dce11668d321fcb82f49e
Bhaskar <negibhaskar19@gmail.com>
2025-11-07 12:26:43 +0530

    Prepared for devlopment

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -4,22 +4,34 @@ import { connectDb } from "./config/db.js";
 import dotenv from "dotenv";
 import rateLimiter from "./middleware/rateLimiter.js";
 import cors from "cors";
+import path from "path";
 
 dotenv.config();
 
 const app = express();
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-  })
-);
+const __dirname = path.resolve();
+if (process.env.NODE_ENV !== "production") {
+  app.use(
+    cors({
+      origin: "http://localhost:5173",
+    })
+  );
+}
 
 app.use(express.json());
 
 const PORT = process.env.PORT || 5000;
+
 app.use(rateLimiter);
 
 app.use("/api", notesRouter);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 connectDb().then(() => {
   app.listen(PORT, () => {
--- Frontend/src/lib/axios.js
+++ Frontend/src/lib/axios.js
@@ -1,1 +1,2 @@
-export const baseURL = "http://localhost:5000/api";
+export const BASE_URL =
+  import.meta.env.MODE === "development" ? "http://localhost:5000/api" : "/api";
